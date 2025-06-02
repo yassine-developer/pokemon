abeccb7d5cdd5777f8d15884b8a1b90c25d75ab7
yassine-developer <mah.yassine1981@gmail.com>
2025-06-02 11:09:38 +0200

    commit

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-async function getpokemon(name){
+async function getListPokemon(page = 1) {
+  try {
+    const offset = (page - 1) * 20;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
-    try{
+getListPokemon();
+
+async function getpokemon(name) {
+
+  try {
 
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data = await response.json();
     console.log(data);
     return data;
 
-    }catch(error){
+  } catch (error) {
 
-        console.error('Error:',error);
-    }
+    console.error('Error:', error);
+  }
 }
 
-getpokemon('Blastoise');
+let currentPage = 1;
+
+// le numero de la page
+function displayCurrentPage() {
+  document.querySelector('#currentPage').textContent = `${currentPage}`;
+}
+
+displayCurrentPage();
+
+async function displayPokemons(page) {
+
+  const pokemons = await getListPokemon(page);
+  console.log(pokemons);
+  const liste = document.querySelector("#pokemonList");
+  liste.textContent = '';
+  pokemons.results.forEach(pokemon => {
+    const li = document.createElement("li");
+    li.textContent = `${pokemon.name} `;
+    liste.append(li);
+  })
+}
+
+displayPokemons(5);
 
 const input = document.getElementById('nameInput');
 const list = document.getElementById('pokemonList');
@@ -31,13 +67,13 @@ input.addEventListener('input', () => {
 });
 
 list.addEventListener('click', async (e) => {
-    if(e.target.tagName === "LI"){
+  if (e.target.tagName === "LI") {
     console.log(e.target.textContent);
     const pokemonName = e.target.textContent;
     const data = await getpokemon(pokemonName);
 
     const div = document.querySelector(".affichage-Resultat");
-    div.innerHTML="";
+    div.innerHTML = "";
 
     const divCard = document.createElement("div");
     divCard.className = "card";
@@ -45,7 +81,7 @@ list.addEventListener('click', async (e) => {
     const divImage = document.createElement("div");
     divImage.className = "card-image";
     const image = document.createElement("img");
-    image.src= data.sprites.front_default;
+    image.src = data.sprites.front_default;
     divImage.append(image);
     divCard.append(divImage);
     div.append(divCard);
@@ -53,25 +89,35 @@ list.addEventListener('click', async (e) => {
     //partie infos
     const divInfo = document.createElement("div");
     divInfo.className = "info-pokemon";
-    const type =document.createElement('p');
-    const taille =document.createElement('p');
+    const type = document.createElement('p');
+    const taille = document.createElement('p');
     const poids = document.createElement('p');
-    const statsList =document.createElement('p');
+    const statsList = document.createElement('p');
     const stats = document.createElement('ul');
     statsList.textContent = "status:";
-    type.textContent = `type: ${data.types.map(t => t.type.name).join(', ')}`;
-    taille.textContent= `Taille: ${data.height*10}cm`;
+    // type.textContent = `type: ${data.types.map(t => t.type.name).join(', ')}`;
+    type.textContent = ''; // vider le contenu d'origine
+    data.types.forEach(t => {
+      const span = document.createElement('span');
+      span.className = `type-${t.type.name}`; // ex: type-fire
+      span.textContent = t.type.name;
+      span.style.marginRight = "0.5rem";
+      span.style.padding = "0.3rem 0.6rem";
+      span.style.borderRadius = "12px";
+      type.appendChild(span);
+    });
+    taille.textContent = `Taille: ${data.height * 10}cm`;
     poids.textContent = `Poids: ${data.weight / 10}kg`;
     data.stats.forEach(
-      t => { 
+      t => {
         const li = document.createElement("li");
         li.textContent = `${t.stat.name} : ${t.base_stat}`;
         stats.append(li);
       });
     statsList.append(stats);
     console.log(stats.textContent);
-    
-    
+
+
     const title = document.createElement("h3");
     title.textContent = data.species.name;
     divInfo.append(title);
@@ -80,6 +126,22 @@ list.addEventListener('click', async (e) => {
     divInfo.append(poids);
     divInfo.append(statsList);
     divCard.append(divInfo);
-    
-    }
+
+  }
 });
+
+document.querySelector('#next').addEventListener('click', () => {
+  if (currentPage < 66) {
+    currentPage++;
+    displayCurrentPage();
+    displayPokemons(currentPage);
+  }
+})
+
+document.querySelector('#prev').addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    displayCurrentPage();
+    displayPokemons(currentPage);
+  }
+})
